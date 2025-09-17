2ac3e2c8096b05c24edd89eed4ef53df178d8bcc
PC <PC@DESKTOP-OSLTILE>
2025-09-17 18:33:45 +0500

    задание 4

---
+++ арифметика/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace арифметика
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 4-хзначное число:");
+
+            var n = int.Parse(Console.ReadLine());
+
+            var a = n / 1000;           
+            var cd = n % 100;         
+            var b = (n / 100) % 10;   
+
+            var result = a * 100 + cd;
+            result = result * 10 + b;
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ мат/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace мат
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите действительное число");
+
+            var x = double.Parse(Console.ReadLine());
+            var y = F(x);
+
+            Console.WriteLine("y = " + y);
+
+        }
+
+        static double F(double x) => (-Math.Pow(x, 3) + (1 / (x * x + 1))) / (1 + (5 / (x * x + x + 1)));
+    }
+}
