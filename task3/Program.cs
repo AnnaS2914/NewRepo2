3dff4628b6737e210a4cb61c7d4f71e101c30f04
PC <PC@DESKTOP-OSLTILE>
2025-09-17 18:33:45 +0500

    задание 3

---
+++ task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 4-хзначное число:");
+
+            var n = int.Parse(Console.ReadLine());
+
+            var x = n / 1000;
+
+            var y = n % 100;
+
+            var result = x * 100 + y;
+
+            Console.WriteLine("Полученное число: " + result);
+        }
+    }
+}
