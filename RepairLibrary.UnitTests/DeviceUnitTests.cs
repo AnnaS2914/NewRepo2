2c42e002336a3fbdac09e975683af15b4681618f
PC <PC@DESKTOP-OSLTILE>
2026-04-30 02:04:41 +0500

    задание 13 сделано

---
+++ RepairLibrary.UnitTests/DeviceUnitTests.cs
@@ -0,0 +1,55 @@
+﻿using DeviceLibrary;
+using NUnit.Framework;
+using System;
+
+namespace DeviceLibrary.UnitTests
+{
+    [TestFixture]
+    public class DeviceUnitTests
+    {
+        private Device CreateTestDevice()
+        {
+            return new Device("Стиральная машина", "Bosch", "SN12345678");
+        }
+
+        [Test]
+        public void ConstructorTest()
+        {
+            Device device = new Device("Стиральная машина", "Bosch", "SN12345678");
+            Assert.That(device.Name, Is.EqualTo("Стиральная машина"));
+            Assert.That(device.Manufacturer, Is.EqualTo("Bosch"));
+            Assert.That(device.SerialNumber, Is.EqualTo("SN12345678"));
+        }
+
+        [Test]
+        public void RepairPriceNegativeTest()
+        {
+            Device device = CreateTestDevice();
+            Assert.That(() => device.RepairPrice = -100, Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void GetInfoTest()
+        {
+            Device device = CreateTestDevice();
+            device.RepairType = RepairType.Paid;
+            device.FaultDescription = "Не греет воду";
+            device.RepairPrice = 2500;
+            device.MasterFullName = "Иванов И.И.";
+
+            string[] info = device.GetInfo();
+
+            Assert.That(info.Length, Is.EqualTo(2));
+            Assert.That(info[0], Is.EqualTo("Стиральная машина (Bosch), серийный номер: SN12345678"));
+
+            string repairTypeStr;
+            if (device.RepairType == RepairType.Guarantee)
+                repairTypeStr = "гарантийный";
+            else
+                repairTypeStr = "оплачиваемый";
+
+            string expected = $"Тип ремонта: {repairTypeStr}, неисправность: {device.FaultDescription}, цена: {device.RepairPrice:C}, мастер: {device.MasterFullName}";
+            Assert.That(info[1], Is.EqualTo(expected));
+        }
+    }
+}
